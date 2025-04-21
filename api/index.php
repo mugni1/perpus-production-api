cc9f0df47a1719395ab79fb7a41926637bbb4f5c
mugni1 <asepabdulmugni22@gmail.com>
2025-04-21 19:10:18 +0700

    modified

--- api/index.php
+++ api/index.php
@@ -1,3 +1,17 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Maintenance mode
+if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Autoload
+require __DIR__ . '/../vendor/autoload.php';
+
+// Bootstrap Laravel dan handle request
+(require_once __DIR__ . '/../bootstrap/app.php')
+    ->handleRequest(Request::capture());
